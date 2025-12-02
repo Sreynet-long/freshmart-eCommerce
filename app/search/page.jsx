57b8958fc21e2304b2515536770feedcb64708aa
Sreynet-long <sreynetlong168@gmail.com>
2025-12-02 16:46:06 +0700

    12/2/25

--- app/search/page.jsx
+++
@@ -1,8 +0,0 @@
-import { Suspense } from "react";
-import SearchResults from "./SearchResults";
-
-export default function page(){
-  <Suspense fallback={<div>Loading search ...</div>}>
-    <SearchResults/>
-  </Suspense>
-}
